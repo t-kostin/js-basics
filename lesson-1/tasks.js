4af1ad31ab18038e8b261dfc92594c758a3e6d52
t-kostin <timur.kostin@gmai.com>
2021-06-04 15:57:28 +0300

    Tasks from lesson #1

---
+++ lesson-1/tasks.js
@@ -0,0 +1,20 @@
+// Task 1
+let tempCelsius = 100
+let tempFarenheit = tempCelsius * 9 / 5 + 32
+alert(tempFarenheit)
+
+// Task 2
+let name_
+let admin
+name_ = 'Vasily'
+admin = name_
+alert(admin)
+
+// Task 3
+alert(1000 + '108') // Concatenation 
+
+/* Task 4
+Атрибут defer позволяет произвести запуск скрипта после загрузки и парсинга всей страницы html.
+
+Атрибут async позволяет произвести запуск скрипта параллельно с парсингом браузером страницы html (без синхронизации).
+*/
