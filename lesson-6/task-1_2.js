9595b996bfa80f96731bcbce82375b6b96f52fb9
t-kostin <timur.kostin@gmai.com>
2021-06-28 16:47:52 +0300

    working with template strings

--- lesson-6/task-1_2.js
+++ lesson-6/task-1_2.js
@@ -1,4 +1,4 @@
-/* 
+/*
 1. Доработать модуль корзины:
     a. Добавлять в объект корзины выбранные товары по клику на кнопке «Купить»
        без перезагрузки страницы
@@ -7,7 +7,7 @@
 2 У товара может быть несколько изображений. Нужно:
     a. Реализовать функционал показа полноразмерных картинок товара в
        модальном окне
-    b. Реализовать функционал перехода между картинками внутри модального 
+    b. Реализовать функционал перехода между картинками внутри модального
        окна ("листалка")
 */
 'use strict';
@@ -186,60 +186,29 @@ const basket = {
         }
     },
 
+    tableTemplate: function (item, quantity, price, total) {
+        return `<div class="basket__position">
+        <div class="basket__item">${item}</div>
+        <div class="basket__quantity">${quantity}</div>
+        <div class="basket__price">${price}</div>
+        <div class="basket__total">${total}</div>
+        </div>`;
+    },
+
     addTotal: function (basketElement) {
-        const basketPosition = document.createElement('div');
-        basketPosition.className = 'basket__position';
 
-        const basketItem = document.createElement('div');
-        basketItem.className = 'basket__item';
-        basketItem.textContent = 'Basket Total:';
-
-        const basketQuantity = document.createElement('div');
-        basketQuantity.className = 'basket__quantity';
-        basketQuantity.textContent = this.itemsQuantity();
-
-        const basketPrice = document.createElement('div');
-        basketPrice.className = 'basket__price';
-        basketPrice.textContent = '';
-
-        const basketTotal = document.createElement('div');
-        basketTotal.className = 'basket__total';
-        basketTotal.textContent = this.totalPrice();
-
-        const basketButton = document.createElement('div');
-        basketButton.className = 'basket__button';
-        basketButton.textContent = '';
-
-        basketPosition.append(
-            basketItem,
-            basketQuantity,
-            basketPrice,
-            basketTotal,
-            basketButton
+        basketElement.insertAdjacentHTML(
+            'beforeend',
+            this.tableTemplate(
+                'Basket Total',
+                this.itemsQuantity(),
+                '',
+                this.totalPrice()
+            )
         );
-
-        basketElement.append(basketPosition);
     },
 
     addPosition: function (basketElement, item) {
-        const basketPosition = document.createElement('div');
-        basketPosition.className = 'basket__position';
-
-        const basketItem = document.createElement('div');
-        basketItem.className = 'basket__item';
-        basketItem.textContent = item.model;
-
-        const basketQuantity = document.createElement('div');
-        basketQuantity.className = 'basket__quantity';
-        basketQuantity.textContent = item.quantity;
-
-        const basketPrice = document.createElement('div');
-        basketPrice.className = 'basket__price';
-        basketPrice.textContent = item.price;
-
-        const basketTotal = document.createElement('div');
-        basketTotal.className = 'basket__total';
-        basketTotal.textContent = item.quantity * item.price;
 
         const basketButton = document.createElement('div');
         basketButton.className = 'basket__button';
@@ -265,67 +234,54 @@ const basket = {
             minusButton
         );
 
-
-        basketPosition.append(
-            basketItem,
-            basketQuantity,
-            basketPrice,
-            basketTotal,
-            basketButton
+        basketElement.insertAdjacentHTML(
+            'beforeend',
+            this.tableTemplate(
+                item.model,
+                item.quantity,
+                item.price,
+                item.price * item.quantity
+            )
         );
 
-        basketElement.append(basketPosition);
+        basketElement.lastChild.append(basketButton);
 
     },
 
     addHeading: function (basketElement) {
 
-        basketElement.textContent = '';
-        const basketPosition = document.createElement('div');
-        basketPosition.className = 'basket__position';
-
-        const basketItem = document.createElement('div');
-        basketItem.className = 'basket__item';
-        basketItem.textContent = 'Item';
-
-        const basketQuantity = document.createElement('div');
-        basketQuantity.className = 'basket__quantity';
-        basketQuantity.textContent = 'Quantity';
-
-        const basketPrice = document.createElement('div');
-        basketPrice.className = 'basket__price';
-        basketPrice.textContent = 'Price';
-
-        const basketTotal = document.createElement('div');
-        basketTotal.className = 'basket__total';
-        basketTotal.textContent = 'Total';
-
-        const basketButton = document.createElement('div');
-        basketButton.className = 'basket__button';
-        basketButton.textContent = '';
-
-        basketPosition.append(
-            basketItem,
-            basketQuantity,
-            basketPrice,
-            basketTotal,
-            basketButton
+        basketElement.insertAdjacentHTML(
+            'beforeend',
+            this.tableTemplate(
+                'Item',
+                'Quantity',
+                'Price',
+                'Total'
+            )
         );
 
-        basketElement.append(basketPosition);
     },
 };
 
 const catalog = {
     idName: 'catalog',
+    cardTitle: function (product) {
+        return `<h2 class="card__header">${product.model}</h2>`;
+    },
+    cardInfo: function (product) {
+        return `<p>Screen type: ${product.screen.type}</p>
+            <p>Screen size: ${product.screen.size} in</p>
+            <p>Screen resolution: ${product.screen.resolution.horizontal} ×\
+            ${product.screen.resolution.vertical}</p>
+            <p>RAM: ${product.ram}</p>
+            <p>Memory: ${product.memory}</p>
+            <p>Color: ${product.color}</p>
+            <p>Price: ${product.price} ₽</p>`
+    },
     put: function (product) {
         const catalogElement = document.getElementById(this.idName);
         const itemCard = document.createElement('div');
         itemCard.className = 'card';
-
-        const cardModel = document.createElement('h2');
-        cardModel.className = 'card__header';
-        cardModel.textContent = product.model;
 
         const cardImage = document.createElement('img');
         cardImage.className = 'card__image';
@@ -333,41 +289,10 @@ const catalog = {
         cardImage.alt = product.model;
         cardImage.dataset.key = product.key;
         cardImage.addEventListener('click', (event) => this.clickHandler(event));
+        itemCard.append(cardImage);
+        itemCard.insertAdjacentHTML('afterbegin', this.cardTitle(product));
+        itemCard.insertAdjacentHTML('beforeend', this.cardInfo(product));
 
-        const cardScreenSize = document.createElement('p');
-        cardScreenSize.textContent = `Screen size: ${product.screen.size} in`;
-
-        const cardScreenType = document.createElement('p');
-        cardScreenType.textContent = `Screen type: ${product.screen.type}`;
-
-        const cardScreenResolution = document.createElement('p');
-        cardScreenResolution.textContent = `Screen resolution:\
-            ${product.screen.resolution.horizontal} ×\
-            ${product.screen.resolution.vertical}`;
-
-        const cardRam = document.createElement('p');
-        cardRam.textContent = `RAM: ${product.ram}`;
-
-        const cardMemory = document.createElement('p');
-        cardMemory.textContent = `Memory: ${product.memory}`;
-
-        const cardColor = document.createElement('p');
-        cardColor.textContent = `Color: ${product.color}`;
-
-        const cardPrice = document.createElement('p');
-        cardPrice.textContent = `Price: ${product.price} \u20bd`;
-
-        itemCard.append(
-            cardModel,
-            cardImage,
-            cardScreenType,
-            cardScreenSize,
-            cardScreenResolution,
-            cardRam,
-            cardMemory,
-            cardColor,
-            cardPrice,
-        );
         if (product.quantity > 0) {
             const cardButton = document.createElement('button');
             cardButton.textContent = 'Add to Basket';
@@ -389,6 +314,11 @@ const catalog = {
 
 const gallery = {
 
+    pointer: null,
+    images: [],
+    wrapper: null,
+    currentImage: null,
+
     init: function () {
         const closeElement = document.querySelector('img.wrapper__close');
         closeElement.addEventListener('click', () => this.close());
@@ -398,35 +328,33 @@ const gallery = {
 
         const rightArrow = document.querySelector('img.wrapper__right');
         rightArrow.addEventListener('click', () => this.right());
+
+        this.currentImage = document.querySelector('img.wrapper__image');
+        this.wrapper = document.querySelector('#gallery');
     },
 
     show: function (gallery) {
         this.images = gallery;
         this.pointer = 0;
-        const imageElement = document.querySelector('img.wrapper__image');
-        imageElement.src = this.images[this.pointer];
-        const galleryElement = document.querySelector('#gallery');
-        galleryElement.classList.toggle('hidden');
+        this.currentImage.src = this.images[this.pointer];
+        this.wrapper.classList.toggle('hidden');
     },
 
     left: function () {
         if (this.pointer > 0) {
-            const imageElement = document.querySelector('img.wrapper__image');
-            imageElement.src = this.images[--this.pointer];
+            this.currentImage.src = this.images[--this.pointer];
         }
     },
 
     right: function () {
         if (this.pointer < (this.images.length - 1)) {
-            const imageElement = document.querySelector('img.wrapper__image');
-            imageElement.src = this.images[++this.pointer];
+            this.currentImage.src = this.images[++this.pointer];
         }
 
     },
 
     close: function () {
-        const galleryElement = document.querySelector('#gallery');
-        galleryElement.classList.toggle('hidden');
+        this.wrapper.classList.toggle('hidden');
     },
 
 };
